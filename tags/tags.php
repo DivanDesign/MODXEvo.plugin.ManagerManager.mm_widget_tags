905ac90c3a74f8499248711bb291b230015d3c9e
Ilyas Ronef <ilyas@divandesign.biz>
2016-09-21 15:29:38 +0500

    * The following parameters was renamed: 	* “delimiter” → “tagDelimiter”. 	* “source” → “sourceFields”. 	* “display_count” → “displayCount”.

--- tags/tags.php
+++ tags/tags.php
@@ -8,9 +8,9 @@
  * @uses ManagerManager plugin 0.6.
  * 
  * @param $fields {comma separated string} - The name(s) of the template variables this should apply to. @required
- * @param $delimiter {string} - The sign that separates tags in the field. Default: ','.
- * @param $source {comma separated string} - The names(s) of the template variables the list of tags should come from. This allows the list of tags to come from a different field than the widget. By default it uses all the TVs listed in “fields” parameter. Default: =$fields.
- * @param $display_count {boolean} - Display the number of documents using each tag (in brackets after it). Default: false.
+ * @param $tagDelimiter {string} - The sign that separates tags in the field. Default: ','.
+ * @param $sourceFields {comma separated string} - The names(s) of the template variables the list of tags should come from. This allows the list of tags to come from a different field than the widget. By default it uses all the TVs listed in “fields” parameter. Default: =$fields.
+ * @param $displayCount {boolean} - Display the number of documents using each tag (in brackets after it). Default: false.
  * @param $roles {comma separated string} - The roles that the widget is applied to (when this parameter is empty then widget is applied to the all roles). Default: ''.
  * @param $templates {comma separated string} - The templates that the widget is applied to (when this parameter is empty then widget is applied to the all templates). Default: ''.
  * 
@@ -22,7 +22,7 @@
  * @copyright 2014
  */
 
-function mm_widget_tags($fields, $delimiter = ',', $source = '', $display_count = false, $roles = '', $templates = ''){
+function mm_widget_tags($fields, $tagDelimiter = ',', $sourceFields = '', $displayCount = false, $roles = '', $templates = ''){
 	if (!useThisRule($roles, $templates)){return;}
 	
 	global $modx;
@@ -42,10 +42,10 @@ function mm_widget_tags($fields, $delimiter = ',', $source = '', $display_count 
 		$fields = makeArray($fields);
 		
 		// And likewise for the data source (if supplied)
-		$source = empty($source) ? $fields : makeArray($source);
+		$sourceFields = empty($sourceFields) ? $fields : makeArray($sourceFields);
 		
-		$source = tplUseTvs($mm_current_page['template'], $source);
-		if ($source == false){return;}
+		$sourceFields = tplUseTvs($mm_current_page['template'], $sourceFields);
+		if ($sourceFields == false){return;}
 		
 		$output .= '//---------- mm_widget_tags :: Begin -----'.PHP_EOL;
 		
@@ -57,12 +57,12 @@ function mm_widget_tags($fields, $delimiter = ',', $source = '', $display_count 
 			$tagsFromAllDocs = $modx->db->makeArray($modx->db->select(
 				'value',
 				$modx->getFullTableName('site_tmplvar_contentvalues'),
-				'tmplvarid IN ('.implode(',', ddTools::unfoldArray($source)).')'
+				'tmplvarid IN ('.implode(',', ddTools::unfoldArray($sourceFields)).')'
 			));
 			
 			$tagsToOutput = array();
 			foreach ($tagsFromAllDocs as $tagsFromAllDocs_item){
-				$tagsFromAllDocs_item = explode($delimiter, $tagsFromAllDocs_item['value']);
+				$tagsFromAllDocs_item = explode($tagDelimiter, $tagsFromAllDocs_item['value']);
 				
 				foreach ($tagsFromAllDocs_item as $tagsFromAllDocs_item_item){
 					$tagsToOutput[trim($tagsFromAllDocs_item_item)]++;
@@ -74,7 +74,7 @@ function mm_widget_tags($fields, $delimiter = ',', $source = '', $display_count 
 			
 			$htmlTagList = '';
 			foreach($tagsToOutput as $tagsToOutput_item_name => $tagsToOutput_item_count){
-				$htmlTagList .= '<li title="Used '.$tagsToOutput_item_count.' times">'.jsSafe($tagsToOutput_item_name).($display_count ? ' ('.$tagsToOutput_item_count.')' : '').'</li>';
+				$htmlTagList .= '<li title="Used '.$tagsToOutput_item_count.' times">'.jsSafe($tagsToOutput_item_name).($displayCount ? ' ('.$tagsToOutput_item_count.')' : '').'</li>';
 			}
 			
 			$htmlTagList = '<ul class="mmTagList" id="'.$fields_item_id.'_tagList">'.$htmlTagList.'</ul>';
@@ -87,7 +87,7 @@ $j("#'.$fields_item_id.'").after(\''.$htmlTagList.'\');
 ';
 			
 			// Initiate the tagCompleter class for this field
-			$output .= 'var '.$fields_item_id.'_tags = new TagCompleter("'.$fields_item_id.'", "'.$fields_item_id.'_tagList", "'.$delimiter.'");'."\n";
+			$output .= 'var '.$fields_item_id.'_tags = new TagCompleter("'.$fields_item_id.'", "'.$fields_item_id.'_tagList", "'.$tagDelimiter.'");'."\n";
 		}
 		
 		$output .= '//---------- mm_widget_tags :: End -----'.PHP_EOL;
